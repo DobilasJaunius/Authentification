a4c5fa95c666b95bd448440a0a86a1c4c7b95d9a
DobilasJaunius <dobilasjaunius@gmail.com>
2025-04-29 22:33:26 +0300

    Clean first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.static('public'));
+
+app.set('view engine', 'ejs');
+
+//connect to mongoDb
+const dbURI = process.env.MONGODB_URI;
+mongoose.connect(dbURI)
+    .then((result) => app.listen(3000))
+    .catch((err) => console.log(err));
+
+//routes
+app.get("/", (req, res) => res.render('home'));
