6ce8aa65d1a4ab822f358c9cacafa206d3ae6e2e
DobilasJaunius <dobilasjaunius@gmail.com>
2025-05-01 22:03:55 +0300

    fifth commit: Sign up page Added error messages to sign up page Sign up page now redirects to main page Removed useless cookie code

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const authRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
 //middleware
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cookieParser());
 
 //view engine
 app.set('view engine', 'ejs');
@@ -23,3 +25,4 @@ mongoose.connect(dbURI)
 app.get("/", (req, res) => res.render('home'));
 
 app.use(authRoutes);
+
