357c595914bf82ef2c16457ab76863a3a1d89287
DobilasJaunius <dobilasjaunius@gmail.com>
2025-05-15 14:27:08 +0300

    sixth commit Made SignUp and Login buttons on main site actually link to signup and login pages Now logging in creates a cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,7 +6,16 @@ const handleErrors = (err) =>{
     console.log(err.message, err.code);
     let errors = { email: '', password: '' };
 
-    
+    //incorrect email
+    if (err.message === 'incorrect email'){
+        errors.email = 'there is no user with that email';
+    }
+
+    //incorrect password
+    if (err.message === 'incorrect password'){
+        errors.password = 'password is incorrect';
+    }
+
     //duplicate error
     if(err.code == 11000){
         errors.email = 'Email is already in use';
@@ -56,8 +65,13 @@ module.exports.login_post = async(req, res) => {
         
     try {
         const user = await User.login(email, password);
+
+        const token = createToken(user._id);
+        res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000}); 
+
         res.status(200).json({user: user._id});            
     } catch (err) {
-        res.status(400).json({});        
+        const errors = handleErrors(err);
+        res.status(400).json({ errors });        
     }
 };
